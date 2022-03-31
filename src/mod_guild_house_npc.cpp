e270e1fbd922afb6bd9f88f561f83b8767dee51d
SoulSeekkor <soulseekkor@live.com>
2022-03-30 19:36:26 -0500

    Updated to use existing Death Knight Trainer.

--- src/mod_guild_house_npc.cpp
+++ src/mod_guild_house_npc.cpp
@@ -63,7 +63,7 @@ public:
         {
         case 2: // Spawn Class Trainer
             ClearGossipMenuFor(player);
-            AddGossipItemFor(player, GOSSIP_ICON_TRAINER, "Death Knight", GOSSIP_SENDER_MAIN, 500034, "Spawn Death Knight Trainer?", GuildHouseTrainer, false);
+            AddGossipItemFor(player, GOSSIP_ICON_TRAINER, "Death Knight", GOSSIP_SENDER_MAIN, 29195, "Spawn Death Knight Trainer?", GuildHouseTrainer, false);
             AddGossipItemFor(player, GOSSIP_ICON_TRAINER, "Druid", GOSSIP_SENDER_MAIN, 26324, "Spawn Druid Trainer?", GuildHouseTrainer, false);
             AddGossipItemFor(player, GOSSIP_ICON_TRAINER, "Hunter", GOSSIP_SENDER_MAIN, 26325, "Spawn Hunter Trainer?", GuildHouseTrainer, false);
             AddGossipItemFor(player, GOSSIP_ICON_TRAINER, "Mage", GOSSIP_SENDER_MAIN, 26326, "Spawn Mage Trainer?", GuildHouseTrainer, false);
@@ -186,7 +186,7 @@ public:
         case 26330: // Shaman
         case 26331: // Warlock
         case 26332: // Warrior
-        case 500034: // Death Knight
+        case 29195: // Death Knight
             cost = GuildHouseTrainer;
             SpawnNPC(action, player);
             break;
